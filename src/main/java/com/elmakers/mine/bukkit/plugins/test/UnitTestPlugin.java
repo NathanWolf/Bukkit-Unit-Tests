3933fc22098283fd31c82e52f9a8ce9e0dbf236f
Nathan Wolf <nathan@elmakers.com>
2014-05-29 09:54:28 -0700

    Check in basic Unit Test bootstrap plugin
    It'll be all branches from here.

---
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -0,0 +1,33 @@
+package com.elmakers.mine.bukkit.plugins.test;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UnitTestPlugin extends JavaPlugin implements Listener
+{
+    private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
+    private static final ChatColor ERROR_PREFIX = ChatColor.RED;
+
+    public void onEnable()
+	{
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+	}
+
+	public void onDisable()
+    {
+    }
+
+    protected void sendMessage(CommandSender sender, String string)
+    {
+        sender.sendMessage(CHAT_PREFIX + string);
+    }
+
+    protected void sendError(CommandSender sender, String string)
+    {
+        sender.sendMessage(ERROR_PREFIX + string);
+    }
+}
