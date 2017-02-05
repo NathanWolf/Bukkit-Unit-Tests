c4efd362e16f480e60b98d083faefd7604da3e22
Nathan Wolf <nathan@elmakers.com>
2017-02-04 20:08:46 -0800

    Add new player interact test plugin

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,33 +1,37 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UnitTestPlugin extends JavaPlugin implements Listener
-{
+public class UnitTestPlugin extends JavaPlugin implements Listener {
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
-    public void onEnable()
-	{
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(this, this);
-	}
-
-	public void onDisable()
-    {
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
     }
 
-    protected void sendMessage(CommandSender sender, String string)
-    {
+    public void onDisable() {
+    }
+
+    protected void sendMessage(CommandSender sender, String string) {
         sender.sendMessage(CHAT_PREFIX + string);
     }
 
-    protected void sendError(CommandSender sender, String string)
-    {
+    protected void sendError(CommandSender sender, String string) {
         sender.sendMessage(ERROR_PREFIX + string);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Block block = event.getClickedBlock();
+        getLogger().info("INTERACT: " + event.getAction() + " on " + (block == null ? "NOTHING" : block.getType()));
     }
 }
