71ade9c995fcd27a941ffc301dddd5d0f16524e6
Nathan Wolf <nathan@elmakers.com>
2017-02-08 14:35:41 -0800

    Add simple kit plugin

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,37 +1,73 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
 import org.bukkit.ChatColor;
-import org.bukkit.block.Block;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UnitTestPlugin extends JavaPlugin implements Listener {
+import java.util.List;
+
+public class UnitTestPlugin extends JavaPlugin implements Listener, CommandExecutor {
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
     public void onEnable() {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(this, this);
+        getCommand("givekit").setExecutor(this);
+        saveDefaultConfig();
     }
 
     public void onDisable() {
     }
-
-    protected void sendMessage(CommandSender sender, String string) {
-        sender.sendMessage(CHAT_PREFIX + string);
-    }
-
-    protected void sendError(CommandSender sender, String string) {
-        sender.sendMessage(ERROR_PREFIX + string);
-    }
-
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Block block = event.getClickedBlock();
-        getLogger().info("INTERACT: " + event.getAction() + " on " + (block == null ? "NOTHING" : block.getType()));
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage("Usage: givekit <kitname>");
+            return true;
+        }
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command may only be used in-game");
+            return true;
+        }
+        Configuration config = getConfig();
+        if (!config.isSet("kits." + args[0])) {
+            sender.sendMessage("No kit named " + args[0]);
+            return true;
+        }
+        Object kitObject = config.get("kits." + args[0]);
+        if (kitObject == null) {
+            sender.sendMessage("Empty kit: " + args[0]);
+            return true;
+        }
+        if (!(kitObject instanceof List)) {
+            sender.sendMessage("Invalid kit: " + args[0]);
+            return true;
+        }
+        List<ItemStack> kitItems = (List<ItemStack>)kitObject;
+        Player player = (Player)sender;
+        
+        // This is assuming you want to actually reset their whole inventory, versus add the items ...
+        for (int index = 0; index < kitItems.size(); index++) {
+            ItemStack item = kitItems.get(index);
+            if (item == null) item = new ItemStack(Material.AIR);
+            player.getInventory().setItem(index, item);
+        }
+        
+        
+        // Otherwise, just add items like this- but in that case having null entries in the config doesn't make much sense.
+        /*
+        for (ItemStack item : kitItems) {
+            if (item != null {
+                player.getInventory().addItem(item);
+            }
+        }
+        */
+        return true;
     }
 }
