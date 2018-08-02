770b2cc9fc1435321114009410ecd88da06d49b7
Nathan Wolf <nathan@elmakers.com>
2018-08-02 14:31:58 -0700

    Add turtle egg PR test plugin

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,33 +1,41 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UnitTestPlugin extends JavaPlugin implements Listener
-{
+public class UnitTestPlugin extends JavaPlugin implements Listener {
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
-    public void onEnable()
-	{
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(this, this);
-	}
-
-	public void onDisable()
-    {
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
     }
 
-    protected void sendMessage(CommandSender sender, String string)
-    {
+    public void onDisable() {
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.PHYSICAL && event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.TURTLE_EGG) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("Hey, get off of the turtle eggs!");
+        }
+    }
+
+    protected void sendMessage(CommandSender sender, String string) {
         sender.sendMessage(CHAT_PREFIX + string);
     }
 
-    protected void sendError(CommandSender sender, String string)
-    {
+    protected void sendError(CommandSender sender, String string) {
         sender.sendMessage(ERROR_PREFIX + string);
     }
 }
