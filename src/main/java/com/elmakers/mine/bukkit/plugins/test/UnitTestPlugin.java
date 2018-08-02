9956e56fb1233877856786a61caf8954297384d9
Nathan Wolf <nathan@elmakers.com>
2018-08-02 16:16:28 -0700

    Add test plugin preventing villagers from opening/closing doors

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,34 +1,45 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UnitTestPlugin extends JavaPlugin implements Listener {
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
+    private CommandSender target;
 
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this, this);
+
+        target = Bukkit.getConsoleSender();
     }
 
     public void onDisable() {
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.PHYSICAL && event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.TURTLE_EGG) {
+    @EventHandler
+    public void onEntityInteract(EntityInteractEvent event) {
+        String message = "Interact from " + event.getEntity().getType() + " on " + event.getBlock().getType();
+        if (event.getEntity().getType() == EntityType.VILLAGER) {
             event.setCancelled(true);
-            event.getPlayer().sendMessage("Hey, get off of the turtle eggs!");
+            message += " DENIED!";
         }
+        sendMessage(target, message);
+    }
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        target = event.getPlayer();
     }
 
     protected void sendMessage(CommandSender sender, String string) {
