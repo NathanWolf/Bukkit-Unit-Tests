c2f183547956b1b62140b635a245b82014aff45d
Nathan Wolf <nathan@elmakers.com>
2020-10-26 15:50:58 -0700

    Add unit test for recipe choice with damage

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,23 +1,47 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("deprecation")
 public class UnitTestPlugin extends JavaPlugin implements Listener
 {
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
     public void onEnable()
-	{
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(this, this);
-	}
+    {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
 
-	public void onDisable()
+        // Register a crafting recipe that lets us turn leather
+        // armor into diamond armor
+        NamespacedKey recipeKey = new NamespacedKey(this, "irondiamondhelmet");
+        ShapedRecipe shapedRecipe = new ShapedRecipe(recipeKey, new ItemStack(Material.DIAMOND_HELMET));
+        shapedRecipe.shape("o", "i");
+        shapedRecipe.setIngredient('o', Material.DIAMOND);
+        List<ItemStack> variants = new ArrayList<>();
+        for (short damage = 0; damage < Material.LEATHER_HELMET.getMaxDurability(); damage++) {
+            ItemStack variant = new ItemStack(Material.LEATHER_HELMET, 1, damage);
+            variants.add(variant);
+        }
+        RecipeChoice.ExactChoice choice = new RecipeChoice.ExactChoice(variants);
+        shapedRecipe.setIngredient('i', choice);
+        getServer().addRecipe(shapedRecipe);
+    }
+
+    public void onDisable()
     {
     }
 
