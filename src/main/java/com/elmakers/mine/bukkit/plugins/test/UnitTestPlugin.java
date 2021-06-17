d7fb318e49debc811db0602a9eb3401ac18c5107
Nathan Wolf <nathan@elmakers.com>
2021-06-16 18:53:46 -0700

    Add entity query plugin

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,14 +1,19 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
@@ -16,10 +21,12 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UnitTestPlugin extends JavaPlugin implements Listener, TabExecutor {
-    private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
-    private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
-    private Set<UUID> loadedEntities = new HashSet<>();
+    private Map<EntityType, Integer> loadedEntities = new HashMap<>();
+    private int chunkLoads = 0;
+    private int emptyChunkLoads = 0;
+    private int persistentEntityLoads = 0;
+    private int entityLoads = 0;
 
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
@@ -47,28 +54,81 @@ public class UnitTestPlugin extends JavaPlugin implements Listener, TabExecutor 
     }
 
     private boolean checkEntities(CommandSender sender, String[] args) {
+        if (chunkLoads > 0) {
+            sender.sendMessage(ChatColor.AQUA + "Mobs spawned in "
+                + ChatColor.YELLOW + chunkLoads + ChatColor.AQUA + " chunk loads ("
+                + ChatColor.RED + emptyChunkLoads + ChatColor.AQUA + " empty)");
+            printEntities(sender);
+        }
+        reset();
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                trackEntity(entity);
+            }
+        }
+        sender.sendMessage(ChatColor.AQUA + "Current entities:");
+        printEntities(sender);
+        reset();
+
         return true;
     }
 
+    private void printEntities(CommandSender sender) {
+        sender.sendMessage(" " + ChatColor.AQUA + "Persistent"
+                + ChatColor.WHITE + ": " + ChatColor.BLUE + persistentEntityLoads
+                + ChatColor.WHITE + " / " + ChatColor.BLUE + entityLoads);
+        for (Map.Entry<EntityType, Integer> entry : loadedEntities.entrySet()) {
+            sender.sendMessage(" " + ChatColor.GREEN + entry.getKey().name()
+                    + ChatColor.WHITE + ": " + ChatColor.BLUE + entry.getValue());
+        }
+    }
+
+    private void reset() {
+        chunkLoads = 0;
+        persistentEntityLoads = 0;
+        emptyChunkLoads = 0;
+        loadedEntities.clear();
+    }
+
     private boolean spawnPersistent(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command may only be used in-game");
+            return true;
+        }
+        Location location = ((Player)sender).getLocation();
+        Entity villager = location.getWorld().spawnEntity(location, EntityType.VILLAGER);
+        villager.setPersistent(true);
+        sender.sendMessage(ChatColor.DARK_AQUA + "Spawned a persistent villager at your location");
         return true;
     }
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent event) {
+        chunkLoads++;
+        Entity[] entities = event.getChunk().getEntities();
+        if (entities.length == 0) {
+            emptyChunkLoads++;
+        } else {
+            for (Entity entity : entities) {
+                trackEntity(entity);
+            }
+        }
+    }
 
+    private void trackEntity(Entity entity) {
+        entityLoads++;
+        Integer current = loadedEntities.get(entity.getType());
+        if (current == null) {
+            current = 0;
+        }
+        loadedEntities.put(entity.getType(), current + 1);
+        if (!(entity instanceof Player) && entity.isPersistent()) {
+            persistentEntityLoads++;
+        }
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         return null;
-    }
-
-    protected void sendMessage(CommandSender sender, String string) {
-        sender.sendMessage(CHAT_PREFIX + string);
-    }
-
-    protected void sendError(CommandSender sender, String string) {
-        sender.sendMessage(ERROR_PREFIX + string);
     }
 }
