87bb123b47acf5b18a37b3f5d13aea5bd2d88cf8
Nathan Wolf <nathan@elmakers.com>
2021-06-16 18:28:52 -0700

    Stub out plugin class

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,12 +1,17 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,9 +19,15 @@ public class UnitTestPlugin extends JavaPlugin implements Listener, TabExecutor 
     private static final ChatColor CHAT_PREFIX = ChatColor.AQUA;
     private static final ChatColor ERROR_PREFIX = ChatColor.RED;
 
+    private Set<UUID> loadedEntities = new HashSet<>();
+
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this, this);
+        getCommand("checkentities").setExecutor(this);
+        getCommand("checkentities").setTabCompleter(this);
+        getCommand("spawnpersistent").setExecutor(this);
+        getCommand("spawnpersistent").setTabCompleter(this);
     }
 
     public void onDisable() {
@@ -24,7 +35,28 @@ public class UnitTestPlugin extends JavaPlugin implements Listener, TabExecutor 
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        String commandName = command.getName();
+        switch (commandName) {
+            case "checkentities":
+                return checkEntities(sender, args);
+            case "spawnpersistent":
+                return spawnPersistent(sender, args);
+            default:
+                return false;
+        }
+    }
+
+    private boolean checkEntities(CommandSender sender, String[] args) {
+        return true;
+    }
+
+    private boolean spawnPersistent(CommandSender sender, String[] args) {
+        return true;
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+
     }
 
     @Override
