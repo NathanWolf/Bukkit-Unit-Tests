3f7ba908939076256ee2246b5dc16055c5a9f022
Nathan Wolf <nathan@elmakers.com>
2022-02-28 10:59:05 -0800

    Add config list saving example

--- src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/test/UnitTestPlugin.java
@@ -1,11 +1,17 @@
 package com.elmakers.mine.bukkit.plugins.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +23,20 @@ public class UnitTestPlugin extends JavaPlugin implements Listener, TabExecutor 
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(this, this);
+
+        YamlConfiguration newConfig = new YamlConfiguration();
+        List<ConfigurationSection> configList = new ArrayList<>();
+        MemoryConfiguration newSection = new MemoryConfiguration();
+        newSection.set("something", "value");
+        configList.add(newSection);
+        newConfig.set("list", configList);
+        try {
+            File outputFile = new File(getDataFolder(), "data.yml");
+            newConfig.save(outputFile);
+            getLogger().info("Wrote to: " + outputFile.getAbsolutePath());
+        } catch (IOException ex) {
+            getLogger().severe("Could not save file: " + ex.getMessage());
+        }
     }
 
     public void onDisable() {
